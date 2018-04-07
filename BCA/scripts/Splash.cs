276a306a61563f26f1be1acb6e17bf4474793ef2
D W <h.k.lieraenoire@gmail.com>
2018-04-07 18:06:41 +1000

    fixing c# project build failure

---
+++ BCA/scripts/Splash.cs
@@ -0,0 +1,29 @@
+using System;
+using Godot;
+
+public class Splash : Node {
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+    private AnimationPlayer animation;
+    public override void _Ready () {
+        animation = GetNode("start_anim") as AnimationPlayer;
+        if (!animation.IsPlaying()) {
+            animation.Play("title_intro");
+        }
+        // Called every time the node is added to the scene.
+        // Initialization here
+
+    }
+    private void _on_start_pressed () {
+        // Replace with function body
+		GD.Print("dfs");
+    }
+
+    //    public override void _Process(float delta)
+    //    {
+    //        // Called every frame. Delta is time since last frame.
+    //        // Update game logic here.
+    //        
+    //    }
+}
